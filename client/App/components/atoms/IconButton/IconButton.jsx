86a1788f9390a118cadc02e41860d283decf3592
pau-cardoso <paulinacardoso7@gmail.com>
2022-10-04 13:49:37 -0500

    Add IconButton component

---
+++ client/App/components/atoms/IconButton/IconButton.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, View } from 'react-native';
+import { neutral, primary } from '../../config/colors';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function IconButton({ onPress, iconName, size, color, type }) {
+  const buttonStyle = [styles.container];
+  if (type === 'contained') {
+    buttonStyle.push(styles.containedButton);
+  }
+
+  return(
+    <View onPress={onPress} style={buttonStyle}>
+      <Ionicons name={iconName} size={size} color={color} />
+    </View>);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: 'center',
+    borderRadius: 50,
+    padding: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
+    aspectRatio: 1,
+  },
+  containedButton: {
+    backgroundColor: primary.s100,
+  }
+});
+
+IconButton.defaultProps = {
+  onPress: () => {},
+  size: 24,
+  color: neutral.s300,
+  type: 'default',
+};
+
+IconButton.propTypes = {
+  onPress: PropTypes.func,
+  iconName: PropTypes.string,
+  type: PropTypes.oneOf(['default', 'contained']),
+  size: PropTypes.number,
+  color: PropTypes.string,
+};
---
+++ client/App/components/atoms/IconButton/IconButton.stories.js
@@ -0,0 +1,14 @@
+import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react-native';
+import React from 'react';
+import { primary } from '../../config/colors';
+import IconButton from './IconButton';
+
+storiesOf('IconButton', module)
+  .add('Default', () => (
+    <IconButton onPress={action('clicked-text')} iconName='add' size={32} />
+  ))
+  .add('Contained', () => (
+    <IconButton onPress={action('clicked-text')} iconName='add' type='contained' color={primary.brand}  size={32} />
+  ))
+;
