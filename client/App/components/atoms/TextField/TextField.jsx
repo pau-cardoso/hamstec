765e9964007eba84779e845f1c687c4365f7e2b1
pau-cardoso <paulinacardoso7@gmail.com>
2022-10-06 14:37:27 -0500

    Fix get value of TextField in parent components

--- client/App/components/atoms/TextField/TextField.jsx
+++ client/App/components/atoms/TextField/TextField.jsx
@@ -7,14 +7,7 @@ import {fontSize} from '../../config/typography';
 import TextPairing from '../TextPairing/TextPairing';
 import IconButton from '../IconButton/IconButton';
 
-function onChangeInput(event) {
-
-}
-
 export default function TextField(props) {
-	const [quantity, setQuantity] = React.useState("1");
-	const [text, setText] = React.useState("");
-
 	const inputStyle = [styles.input];
 	if (props.type === 'quantity') {
 		inputStyle.push(fontSize.x60);
@@ -25,19 +18,17 @@ export default function TextField(props) {
 			<TextPairing style={styles.spacing} text={props.title} type='medium' size={14} color='s400' />
 			<View style={styles.inputContainer}>
 				{props.type === 'quantity'?
-					<IconButton onPress={() => setQuantity((parseInt(quantity)-1).toString())} iconName='remove' size={32} />
+					<IconButton onPress={() => props.onChangeText((parseInt(props.value)-1).toString())} iconName='remove' size={32} />
 				: null}
 
 				<TextInput
 					style={inputStyle}
-					onChangeText={props.type === 'quantity'? setQuantity: setText}
-					value={props.type === 'quantity'?  quantity: text}
 					placeholderTextColor={neutral.s200}
 					{...props}
 				/>
 
 				{props.type === 'quantity'?
-					<IconButton onPress={() => setQuantity((parseInt(quantity)+1).toString())} iconName='add' size={32} />
+					<IconButton onPress={() => props.onChangeText((parseInt(props.value)+1).toString())} iconName='add' size={32} />
 				: null}
 			</View>
 		</View>
@@ -69,8 +60,3 @@ const styles = StyleSheet.create({
 		...fontSize.x60
   }
 });
-
-TextField.propTypes = {
-  title: PropTypes.string,
-  isActive: PropTypes.bool,
-};
--- client/App/components/atoms/TextField/TextField.stories.js
+++ client/App/components/atoms/TextField/TextField.stories.js
@@ -1,15 +1,30 @@
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
 import TextField from './TextField';
+import { View } from 'react-native';
+
+function Padre(props) {
+  const [quantity, setQuantity] = React.useState("1");
+  const [text, setText] = React.useState("");
+
+  return (
+    <View>
+      { props.type === 'quantity'?
+        <TextField value={quantity} onChangeText={setQuantity} {...props} />
+        : <TextField value={text} onChangeText={setText} {...props} />
+      }
+    </View>
+	);
+}
 
 storiesOf('InputField', module)
   .add('Text', () => (
-    <TextField title='Zona' placeholder='Zona' />
+    <Padre title='Zona' placeholder='Zona' />
   ))
   .add('Multiline', () => (
-    <TextField title='Observaciones' placeholder='Observaciones' multiline numberOfLines={4} />
+    <Padre title='Observaciones' placeholder='Observaciones' multiline numberOfLines={4} />
   ))
   .add('Quantity', () => (
-    <TextField title='Cantidad' placeholder='Cantidad' type='quantity' keyboardType="numeric" />
+    <Padre title='Cantidad' placeholder='Cantidad' type='quantity' keyboardType="numeric" />
   ))
 ;
