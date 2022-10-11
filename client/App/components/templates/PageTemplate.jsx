0af1dda320b4240dfde0520629ab106fbe829212
pau-cardoso <paulinacardoso7@gmail.com>
2022-10-11 12:38:27 -0500

    Add PageTemplate

---
+++ client/App/components/templates/PageTemplate.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, View } from 'react-native';
+import { neutral } from '../config/colors';
+
+export default function PageTemplate({header, body, bottomNavigation, style}) {
+  return(
+    <View style={[styles.container, style]}>
+      <View style={styles.header}>
+        { header }
+      </View>
+      <View style={styles.body}>
+        { body }
+      </View>
+      <View>
+        { bottomNavigation }
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    width: '100%',
+  },
+  header: {
+    backgroundColor: neutral.white,
+    paddingHorizontal: 32,
+    paddingTop: 20,
+  },
+  body: {
+    paddingHorizontal: 32,
+    paddingTop: 22,
+  }
+});
+
+PageTemplate.propTypes = {
+  header: PropTypes.element,
+  body: PropTypes.element,
+  bottomNavigation: PropTypes.element,
+};
