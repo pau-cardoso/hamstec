be3a90b206503e68c6b727b4ce268d24f733b030
pau-cardoso <paulinacardoso7@gmail.com>
2022-10-19 11:26:35 -0500

    add: AgregarProyecto screen modal

---
+++ client/components/pages/AgregarProyecto/AgregarProyecto.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import ModalTemplate from '../../templates/ModalTemplate';
+import PageHeader from '../../molecules/PageHeader/PageHeader';
+import FormGroup from '../../molecules/FormGroup/FormGroup';
+import TextField from '../../atoms/TextField/TextField';
+
+export default function AgregarProyecto({style}) {
+  const [name, setName] = React.useState("");
+  const [client, setClient] = React.useState(""); // TODO: How to add client
+  const [address, setAddress] = React.useState("");
+  return(
+    <View style={[styles.container, style]}>
+      <ModalTemplate
+        header={
+          <PageHeader
+            title='Agregar proyecto'
+          />
+        }
+        body={
+          // TODO: Add onSave method
+          <FormGroup onPressSave={() => {}} style={{padding: 0}} >
+            <TextField value={name} onChangeText={setName} title='Nombre' placeholder='Nombre' />
+            <TextField value={client} onChangeText={setClient} title='Cliente' placeholder='Cliente' />
+            <TextField value={address} onChangeText={setAddress} title='Dirección' placeholder='Dirección' multiline />
+          </FormGroup>
+        }
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+  },
+});
