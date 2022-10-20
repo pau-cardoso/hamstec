fbe2003c8d767cdf8692cf24e78728effe77876e
pau-cardoso <paulinacardoso7@gmail.com>
2022-10-20 18:33:07 -0500

    refactor: design nits in BottomNavigation

---
+++ client/Theme.js
@@ -0,0 +1,13 @@
+import { neutral, primary } from './components/config/colors';
+
+export const HamstecTheme = {
+  dark: false,
+  colors: {
+    primary: primary.brand,
+    background: neutral.s050,
+    card: neutral.white,
+    text: neutral.s900,
+    border: neutral.s300,
+    notification: neutral.s300,
+  },
+};
--- client/components/molecules/BottomNavigation/BottomNavigation.jsx
+++ client/components/molecules/BottomNavigation/BottomNavigation.jsx
@@ -6,72 +6,84 @@ import { neutral, primary } from '../../config/colors';
 export default function BottomNavigation({ state, descriptors, navigation }) {
   return (
     <View style={styles.container}>
-      {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-            ? options.title
-            : route.name;
+      <View style={styles.bottomNav}>
+        {state.routes.map((route, index) => {
+          const { options } = descriptors[route.key];
+          const label =
+            options.tabBarLabel !== undefined
+              ? options.tabBarLabel
+              : options.title !== undefined
+              ? options.title
+              : route.name;
 
-        let iconName;
-        switch (route.name) {
-          case 'Home':
-            iconName = 'home';
-            break;
+          let iconName;
+          switch (route.name) {
+            case 'Home':
+              iconName = 'home';
+              break;
 
-          case 'Proyectos':
-            iconName = 'list';
-            break;
+            case 'Proyectos':
+              iconName = 'list';
+              break;
 
-          default:
-            iconName = 'home';
-            break;
-        }
-
-        const isFocused = state.index === index;
-
-        const onPress = () => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-            canPreventDefault: true,
-          });
-
-          if (!isFocused && !event.defaultPrevented) {
-            // The `merge: true` option makes sure that the params inside the tab screen are preserved
-            navigation.navigate({ name: route.name, merge: true });
+            default:
+              iconName = 'home';
+              break;
           }
-        };
 
-        const onLongPress = () => {
-          navigation.emit({
-            type: 'tabLongPress',
-            target: route.key,
-          });
-        };
+          const isFocused = state.index === index;
 
-        return (
-          <IconButton
-            key={index}
-            onPress={onPress }
-            iconName={iconName}
-            color={isFocused ? primary.brand : neutral.s300}
-            type={isFocused? 'contained' : 'default'} />
-        );
-      })}
+          const onPress = () => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (!isFocused && !event.defaultPrevented) {
+              // The `merge: true` option makes sure that the params inside the tab screen are preserved
+              navigation.navigate({ name: route.name, merge: true });
+            }
+          };
+
+          const onLongPress = () => {
+            navigation.emit({
+              type: 'tabLongPress',
+              target: route.key,
+            });
+          };
+
+          return (
+            <IconButton
+              key={index}
+              onPress={onPress }
+              iconName={iconName}
+              color={isFocused ? primary.brand : neutral.s300}
+              type={isFocused? 'contained' : 'default'} />
+          );
+        })}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingBottom: 18,
+    width: '100%',
+    backgroundColor: neutral.s050,
+  },
+  bottomNav: {
     flexDirection: 'row',
     alignSelf: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
     paddingVertical: 16,
     borderRadius: 50,
+    width: '100%',
+    backgroundColor: neutral.white,
   },
 });
