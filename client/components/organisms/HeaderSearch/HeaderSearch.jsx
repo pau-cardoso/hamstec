544a9a669bc546fabbedaa6dcadcb79b21fde388
pau-cardoso <paulinacardoso7@gmail.com>
2022-10-20 16:58:45 -0500

    add: more header props in HeaderSearch

--- client/components/organisms/HeaderSearch/HeaderSearch.jsx
+++ client/components/organisms/HeaderSearch/HeaderSearch.jsx
@@ -4,14 +4,19 @@ import { StyleSheet, View } from 'react-native';
 import PageHeader from '../../molecules/PageHeader/PageHeader'
 import SearchBar from '../../molecules/SearchBar/SearchBar'
 
-export default function HeaderSearch({title, searchPhrase, setSearchPhrase, style}) {
+export default function HeaderSearch({title, searchPhrase, setSearchPhrase, onPressFilter, onPressBackButton, onRightButtonClick, rightButtonIcon, style}) {
   return(
     <View style={[styles.container, style]}>
-      <PageHeader style={styles.gap} title={title} />
+      <PageHeader
+        style={styles.gap}
+        title={title}
+        onRightButtonClick={onRightButtonClick}
+        rightButtonIcon={rightButtonIcon}
+        onPressBackButton={onPressBackButton} />
       <SearchBar
         searchPhrase={searchPhrase}
         setSearchPhrase={setSearchPhrase}
-        onPressFilter={() => {}} />
+        onPressFilter={onPressFilter} />
     </View>
   );
 }
