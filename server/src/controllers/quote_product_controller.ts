33e6608245bd6603b1f67c7af7378f80ae41a9ec
pau-cardoso <paulinacardoso7@gmail.com>
2022-11-07 13:53:17 -0600

    add: quote summary cost query

--- server/src/controllers/quote_product_controller.ts
+++ server/src/controllers/quote_product_controller.ts
@@ -13,7 +13,22 @@ export async function getQuoteProduct(request, response) {
   return response.send(results)
 }
 
+// TODO: add vehicle cost and update total and utility with it
 export async function getProductsByQuote(request, response) {
+  const quoteSummary = await AppDataSource.getRepository(QuoteProduct)
+    .createQueryBuilder("quoteProduct")
+    .leftJoin('quoteProduct.quote', 'quote')
+    .leftJoin('quoteProduct.product', 'product')
+    .where('quoteProduct.quote = :id_quote', { id_quote: request.params.id_quote })
+    .andWhere('quoteProduct.phase = :phase', { phase: 'COTIZACION' })
+    .select('SUM(product.public_price * quoteProduct.quantity)', 'total')
+    .addSelect('SUM(product.public_price * quoteProduct.quantity) * 0.8', 'anticipo')
+    .addSelect('SUM(product.public_price * quoteProduct.quantity) * 0.2', 'instalacion')
+    .addSelect('SUM(product.price)', 'cost')
+    .addSelect('SUM(product.installation)', 'installation')
+    .addSelect('SUM(product.utility)', 'utility')
+    .getRawOne();
+
   const products = await AppDataSource.getRepository(QuoteProduct).find({
     relations: {
       quote: true,
@@ -31,7 +46,7 @@ export async function getProductsByQuote(request, response) {
         id: "ASC"
       }
     }
-  })
+  });
 
   const productResult = {};
   let currentSection = -1;
@@ -43,7 +58,9 @@ export async function getProductsByQuote(request, response) {
     productResult[currentSection].push(product);
   });
 
-  return response.send(productResult);
+  const result = [productResult, quoteSummary];
+
+  return response.send(result);
 }
 
 export async function addQuoteProduct(request, response) {
