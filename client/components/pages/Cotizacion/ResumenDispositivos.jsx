995676cc662465d2440e89586840bb9cb6a75788
pau-cardoso <paulinacardoso7@gmail.com>
2022-12-26 18:58:18 -0500

    refactor: use FlatList instead of ScrollView

--- client/components/pages/Cotizacion/ResumenDispositivos.jsx
+++ client/components/pages/Cotizacion/ResumenDispositivos.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { FlatList, StyleSheet, View } from 'react-native';
 import Card from '../../atoms/Card/Card';
-import Table from '../../molecules/Table/Table';
 import Row from '../../molecules/Table/Row';
 import Cell from '../../molecules/Table/Cell';
 import TextPairing from '../../atoms/TextPairing/TextPairing';
+import { neutral } from '../../config/colors';
 
 const HEADERS = ['Marca', 'Clave', 'Dispositivo', 'Instalado', 'Contratado', 'Diferencia', 'Estado'];
 const FLEX = [1, 1, 2, 1, 1, 1, 1];
@@ -15,6 +14,7 @@ export default function ResumenDispositivos({style, navigation, route}) {
   const [refreshing, setRefreshing] = React.useState(false);
 
   const {quoteId} = route.params;
+  let tableData = new Array(0);
   const url = "http://localhost:3000/quote-product/count/" + quoteId;
 
   useEffect(() => {
@@ -27,7 +27,6 @@ export default function ResumenDispositivos({style, navigation, route}) {
       .finally(setRefreshing(false))
     }, [refreshing]);
 
-  const tableData = [];
   let totalHired = 0;
   let totalInstalled = 0;
 
@@ -45,7 +44,7 @@ export default function ResumenDispositivos({style, navigation, route}) {
         totalInstalled += parseInt(phase.product_count)
       }
     });
-    const row = [];
+    const row = new Array(0);
     row.push(element[0].brand_name);
     row.push(element[0].product_code);
     row.push(element[0].product_name);
@@ -56,51 +55,59 @@ export default function ResumenDispositivos({style, navigation, route}) {
     tableData.push(row);
   }
 
+  const renderItem = ({ item }) => (
+    <View style={styles.cardRow}>
+      <Row style={{marginBottom: 0}}>
+        {item.map((cell, key) => (
+          <Cell key={key} value={cell} flex={FLEX[key]} />
+        ))}
+      </Row>
+    </View>
+  );
+
   return(
     <View style={[styles.container, style]}>
-      <ScrollView style={{paddingHorizontal: 32, paddingTop: 22}}>
-        <Card>
-          <Table>
-            <Row>
-              { HEADERS.map((header, key) => (
+        <FlatList
+          data={Array.from(tableData)}
+          contentContainerStyle={{paddingHorizontal: 32, paddingTop: 22}}
+          renderItem={renderItem}
+          refreshing={refreshing}
+          onRefresh={() => {setRefreshing(true)}}
+          ListHeaderComponent={
+            <View style={[styles.cardRow, {flexDirection: 'row'}]}>
+              {HEADERS.map((header, key) => (
                 <Cell key={key} value={header} header flex={FLEX[key]} />
-                ))}
-            </Row>
-            { tableData.map((row, key) => (
-              <Row key={key}>
-                {row.map((cell, key) => (
-                  <Cell key={key} value={cell} flex={FLEX[key]} />
-                ))}
-              </Row>
-            ))}
-          </Table>
-        </Card>
-        <View style={styles.cards}>
-          <Card style={{marginTop: 24}}>
-            <TextPairing text='Total dispositivos' type='medium' size={24} style={{marginHorizontal: 64, marginBottom: 8}} />
-            <View style={styles.textRow}>
-              <TextPairing text='Instalado' type='medium' size={16} />
-              <TextPairing text={totalInstalled} size={16} />
+              ))}
             </View>
-            <View style={styles.textRow}>
-              <TextPairing text='Contratado' type='medium' size={16} />
-              <TextPairing text={totalHired} size={16} />
+            }
+          ListFooterComponent={
+            <View style={styles.cards}>
+              <Card style={{marginTop: 24}}>
+                <TextPairing text='Total dispositivos' type='medium' size={24} style={{marginHorizontal: 64, marginBottom: 8}} />
+                <View style={styles.textRow}>
+                  <TextPairing text='Instalado' type='medium' size={16} />
+                  <TextPairing text={totalInstalled} size={16} />
+                </View>
+                <View style={styles.textRow}>
+                  <TextPairing text='Contratado' type='medium' size={16} />
+                  <TextPairing text={totalHired} size={16} />
+                </View>
+                <View style={styles.textRow}>
+                  <TextPairing text='Diferencia' type='medium' size={16} />
+                  <TextPairing text={totalInstalled-totalHired} size={16} />
+                </View>
+                <View style={styles.textRow}>
+                  <TextPairing text='Estado' type='medium' size={16} />
+                  <TextPairing text={(totalHired-totalInstalled) > 0? 'Sobrante' : (totalHired-totalInstalled) < 0? 'Faltante' : ''} size={16} />
+                </View>
+                <View style={styles.textRow}>
+                  <TextPairing text='Diferencia MXN' type='medium' size={16} />
+                  <TextPairing text='' size={16} />
+                </View>
+              </Card>
             </View>
-            <View style={styles.textRow}>
-              <TextPairing text='Diferencia' type='medium' size={16} />
-              <TextPairing text={totalInstalled-totalHired} size={16} />
-            </View>
-            <View style={styles.textRow}>
-              <TextPairing text='Estado' type='medium' size={16} />
-              <TextPairing text={(totalHired-totalInstalled) > 0? 'Sobrante' : (totalHired-totalInstalled) < 0? 'Faltante' : ''} size={16} />
-            </View>
-            <View style={styles.textRow}>
-              <TextPairing text='Diferencia MXN' type='medium' size={16} />
-              <TextPairing text='' size={16} />
-            </View>
-          </Card>
-        </View>
-      </ScrollView>
+          }
+        />
     </View>
   );
 }
@@ -123,5 +130,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     alignContent: 'center',
     alignSelf: 'center',
+    marginBottom: 16,
   },
+  cardRow: {
+    padding: 8,
+    paddingHorizontal: 16,
+    marginBottom: 4,
+    backgroundColor: neutral.white,
+    borderRadius: 10,
+  }
 });
