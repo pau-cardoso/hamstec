3ba5920dff269bd3ce72f96c834c25f9423a36b4
pau-cardoso <paulinacardoso7@gmail.com>
2023-01-17 11:21:47 -0600

    add: ignore messages

--- client/App.js
+++ client/App.js
@@ -4,6 +4,10 @@ import 'react-native-gesture-handler';
 import AppLoading from "expo-app-loading";
 import { Jost_300Light, Jost_400Regular, Jost_500Medium, Jost_600SemiBold, useFonts } from '@expo-google-fonts/jost';
 import Navigation from './Navigation';
+import { LogBox } from 'react-native';
+
+// Ignore all log notifications:
+LogBox.ignoreAllLogs();
 
 export default function App() {
   let [fontsLoaded] = useFonts({
